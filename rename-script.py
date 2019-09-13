f071bda94422c9727c7ac17485c5dbea250be714
unknown <colbymehmen@gmail.com>
2019-09-13 13:22:46 -0600

    init

---
+++ rename-script.py
@@ -0,0 +1,16 @@
+# This program given                            |
+#   1. A file containing multiple '.PRG' files  |
+#   named [unique Identifier].prg               |
+#   2. A dictionary of [unique Identifier]      |
+#   3. A parallel dicitonary of watch names     |
+# Will rename all files with watch name         |
+#-----------------------------------------------|
+
+filename {
+        " ", " " ;
+
+        
+        
+        
+        
+        }
